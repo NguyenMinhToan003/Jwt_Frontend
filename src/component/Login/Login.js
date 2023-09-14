715c1b53698431c6f4900091711d457f29adb526
Toan <nguyentoan04.0003@gmail.com>
2023-09-14 22:34:10 +0700

    #9.2 complete -> Learn React Hook vs Redux :))

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./Login.scss";
+import { FaGoogle } from "react-icons/fa";
 class Login extends React.Component {
   render() {
     return (
-      <div>
-        <div>
-          <div>Login</div>
-          <button className="btn btn-primary">Login</button>
-        </div>
-      </div>
+      <a
+        className="btn btn-primary align-center"
+        style={{ backgroundColor: "#3b5998" }}
+        href="#!">
+        <FaGoogle />
+      </a>
     );
   }
 }
--- src/component/Navigation/Nav.js
+++ src/component/Navigation/Nav.js
@@ -4,21 +4,28 @@ import { NavLink } from "react-router-dom";
 class Nav extends React.Component {
   render() {
     return (
-      <ul>
-        <li>
-          <NavLink to="/" exact>
-            Home
+      <ul className="navbar">
+        <div className="option">
+          <li>
+            <NavLink to="/" exact>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/news">News</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+        </div>
+        <div className="login btn-floating">
+          <NavLink to="/login" className="btn btn-outline-success me-2">
+            Login
           </NavLink>
-        </li>
-        <li>
-          <NavLink to="/news">News</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact">Contact</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
+        </div>
       </ul>
     );
   }
